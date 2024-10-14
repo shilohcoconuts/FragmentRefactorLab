2a2758ccfcf9e74fdd38d7aa61650cbffeb0e6d6
shilohcoconuts <shilohpalmer57@gmail.com>
2024-10-14 15:43:52 -0400

    done :)

---
+++ app/src/main/java/edu/temple/fragmentrefactor/ActiveFragment.kt
@@ -0,0 +1,41 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class ActiveFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val layout = inflater.inflate(R.layout.fragment_active, container, false)
+
+        val displayTextView = layout.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = layout.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = layout.findViewById<Button>(R.id.changeButton)
+
+         changeButton.setOnClickListener {
+        val name = nameEditText.text
+
+          displayTextView.text = if (name.isNotBlank()) {
+        "Hello, $name!"
+         } else {
+             "Please enter your name"
+         }
+        }
+
+        return layout
+    }
+
+
+
+}
